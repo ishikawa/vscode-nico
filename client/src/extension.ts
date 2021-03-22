0ab78615168e2952cba329e815cdc851fdb5186a
Takanori Ishikawa <ishikawa_takanori@mac.com>
2021-03-23 07:38:14 +0900

    multi server

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,65 +1,181 @@
 /* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
+ * This client extension is based on microsoft/vscode-extension-samples.
+ * https://github.com/microsoft/vscode-extension-samples
+ *
+ * License of microsoft/vscode-extension-samples
+ * ---------------------------------------------
+ * Copyright (c) Microsoft Corporation
+ *
+ * All rights reserved.
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
+ * OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  * ------------------------------------------------------------------------------------------ */
-
 import * as path from "path";
-import { workspace, ExtensionContext } from "vscode";
+import {
+  workspace as Workspace,
+  window as Window,
+  ExtensionContext,
+  TextDocument,
+  OutputChannel,
+  WorkspaceFolder,
+  Uri,
+} from "vscode";
 
 import {
   LanguageClient,
   LanguageClientOptions,
-  ServerOptions,
   TransportKind,
 } from "vscode-languageclient/node";
 
-let client: LanguageClient;
+let defaultClient: LanguageClient;
+let clients: Map<string, LanguageClient> = new Map();
 
-export function activate(context: ExtensionContext) {
-  // The server is implemented in node
-  let serverModule = context.asAbsolutePath(
-    path.join("server", "out", "server.js")
-  );
-  // The debug options for the server
-  // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-  let debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
+let _sortedWorkspaceFolders: string[] | undefined;
+function sortedWorkspaceFolders(): string[] {
+  if (_sortedWorkspaceFolders === void 0) {
+    _sortedWorkspaceFolders = Workspace.workspaceFolders
+      ? Workspace.workspaceFolders
+          .map((folder) => {
+            let result = folder.uri.toString();
+            if (result.charAt(result.length - 1) !== "/") {
+              result = result + "/";
+            }
+            return result;
+          })
+          .sort((a, b) => {
+            return a.length - b.length;
+          })
+      : [];
+  }
+  return _sortedWorkspaceFolders;
+}
+Workspace.onDidChangeWorkspaceFolders(
+  () => (_sortedWorkspaceFolders = undefined)
+);
 
-  // If the extension is launched in debug mode then the debug server options are used
-  // Otherwise the run options are used
-  let serverOptions: ServerOptions = {
-    run: { module: serverModule, transport: TransportKind.ipc },
-    debug: {
-      module: serverModule,
-      transport: TransportKind.ipc,
-      options: debugOptions,
-    },
-  };
-
-  // Options to control the language client
-  let clientOptions: LanguageClientOptions = {
-    // Register the server for plain text documents
-    documentSelector: [{ scheme: "file", language: "nico" }],
-    synchronize: {
-      // Notify the server about file changes to '.clientrc files contained in the workspace
-      fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
-    },
-  };
-
-  // Create the language client and start the client.
-  client = new LanguageClient(
-    "languageServerExample",
-    "Language Server Example",
-    serverOptions,
-    clientOptions
-  );
-
-  // Start the client. This will also launch the server
-  client.start();
+function getOuterMostWorkspaceFolder(folder: WorkspaceFolder): WorkspaceFolder {
+  let sorted = sortedWorkspaceFolders();
+  for (let element of sorted) {
+    let uri = folder.uri.toString();
+    if (uri.charAt(uri.length - 1) !== "/") {
+      uri = uri + "/";
+    }
+    if (uri.startsWith(element)) {
+      return Workspace.getWorkspaceFolder(Uri.parse(element))!;
+    }
+  }
+  return folder;
 }
 
-export function deactivate(): Thenable<void> | undefined {
-  if (!client) {
-    return undefined;
+export function activate(context: ExtensionContext) {
+  let module = context.asAbsolutePath(path.join("server", "out", "server.js"));
+  let outputChannel: OutputChannel = Window.createOutputChannel(
+    "lsp-multi-server-example"
+  );
+
+  function didOpenTextDocument(document: TextDocument): void {
+    // We are only interested in language mode text
+    if (
+      document.languageId !== "nico" ||
+      (document.uri.scheme !== "file" && document.uri.scheme !== "untitled")
+    ) {
+      return;
+    }
+
+    let uri = document.uri;
+    // Untitled files go to a default client.
+    if (uri.scheme === "untitled" && !defaultClient) {
+      let debugOptions = { execArgv: ["--nolazy", "--inspect=6010"] };
+      let serverOptions = {
+        run: { module, transport: TransportKind.ipc },
+        debug: { module, transport: TransportKind.ipc, options: debugOptions },
+      };
+      let clientOptions: LanguageClientOptions = {
+        documentSelector: [{ scheme: "untitled", language: "nico" }],
+        diagnosticCollectionName: "lsp-multi-server-example",
+        outputChannel: outputChannel,
+      };
+      defaultClient = new LanguageClient(
+        "lsp-multi-server-example",
+        "LSP Multi Server Example",
+        serverOptions,
+        clientOptions
+      );
+      defaultClient.start();
+      return;
+    }
+    let folder = Workspace.getWorkspaceFolder(uri);
+    // Files outside a folder can't be handled. This might depend on the language.
+    // Single file languages like JSON might handle files outside the workspace folders.
+    if (!folder) {
+      return;
+    }
+    // If we have nested workspace folders we only start a server on the outer most workspace folder.
+    folder = getOuterMostWorkspaceFolder(folder);
+
+    if (!clients.has(folder.uri.toString())) {
+      let debugOptions = {
+        execArgv: ["--nolazy", `--inspect=${6011 + clients.size}`],
+      };
+      let serverOptions = {
+        run: { module, transport: TransportKind.ipc },
+        debug: { module, transport: TransportKind.ipc, options: debugOptions },
+      };
+      let clientOptions: LanguageClientOptions = {
+        documentSelector: [
+          {
+            scheme: "file",
+            language: "nico",
+            pattern: `${folder.uri.fsPath}/**/*`,
+          },
+        ],
+        diagnosticCollectionName: "lsp-multi-server-example",
+        workspaceFolder: folder,
+        outputChannel: outputChannel,
+      };
+      let client = new LanguageClient(
+        "lsp-multi-server-example",
+        "LSP Multi Server Example",
+        serverOptions,
+        clientOptions
+      );
+      client.start();
+      clients.set(folder.uri.toString(), client);
+    }
   }
-  return client.stop();
+
+  Workspace.onDidOpenTextDocument(didOpenTextDocument);
+  Workspace.textDocuments.forEach(didOpenTextDocument);
+  Workspace.onDidChangeWorkspaceFolders((event) => {
+    for (let folder of event.removed) {
+      let client = clients.get(folder.uri.toString());
+      if (client) {
+        clients.delete(folder.uri.toString());
+        client.stop();
+      }
+    }
+  });
+}
+
+export function deactivate(): Thenable<void> {
+  let promises: Thenable<void>[] = [];
+  if (defaultClient) {
+    promises.push(defaultClient.stop());
+  }
+  for (let client of clients.values()) {
+    promises.push(client.stop());
+  }
+  return Promise.all(promises).then(() => undefined);
 }
